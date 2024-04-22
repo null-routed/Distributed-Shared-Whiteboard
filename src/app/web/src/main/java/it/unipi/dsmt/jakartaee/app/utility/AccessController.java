3a920b95d23e925879a3a5cf448413462b6c18a0
Niccolò <niccolo.mule@gmail.com>
2024-04-22 17:16:30 +0200

    removed unnecessary code

--- src/app/web/src/main/java/it/unipi/dsmt/jakartaee/app/utility/AccessController.java
+++ src/app/web/src/main/java/it/unipi/dsmt/jakartaee/app/utility/AccessController.java
@@ -1,15 +1,12 @@
 package it.unipi.dsmt.jakartaee.app.utility;
 
 import it.unipi.dsmt.jakartaee.app.dto.LoggedUserDTO;
-import it.unipi.dsmt.jakartaee.app.interfaces.WhiteboardEJB;
-import jakarta.ejb.EJB;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Static class providing methods for controlling user permissions to webapp resources
@@ -19,47 +16,6 @@ import java.util.List;
 public class AccessController {
 
     private static final String LOGGED_USER_ATTRIBUTE = "logged_user";
-
-    /**
-     * Checks if the logged user is allowed to access the requested resource.
-     * It redirects the user to its main page if the user is not allowed or
-     * to the login page if they are not logged.
-     * In these cases, the method returns null.
-     *
-     * @param request HttpServletRequest object
-     * @param response HttpServletResponse object
-     * @param requiredRole required role to access the requested resource
-     * @return stored instance of LoggedUserDTO if the user is logged AND
-     * they're allowed to access the requested resource, null otherwise
-     * @throws IOException if redirection fails
-     */
-/*
-    public static @Nullable LoggedUserDTO checkAccess (
-            @NotNull HttpServletRequest request,
-            @NotNull HttpServletResponse response,
-            @NotNull UserRole requiredRole) throws IOException
-    {
-        // Check is arguments are valid
-        if (requiredRole == UserRole.invalid) {
-            throw new IllegalArgumentException("Invalid required role");
-        }
-
-        // Get currently logged user's information
-        LoggedUserDTO loggedUser = getLoggedUserWithRedirect(request, response);
-        if (loggedUser == null) {
-            return null;
-        }
-
-        // Check if current user role against required role to access the resource
-        if (loggedUser.getRole() != requiredRole) {
-            // User not allowed to access
-            ClientRedirector.redirectToPortalPage(request, response, loggedUser.getRole());
-            return null;
-        }
-
-        return loggedUser;
-    }
-*/
 
     /**
      * Get a LoggedUserDTO object corresponding to the currently logged user.
